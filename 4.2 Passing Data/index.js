e6f8273739778b118421056717784ec4172660b0
Nikola Milanović <nikola.milanovic.dev@gmail.com>
2023-12-07 15:53:09 +0100

    Code refactor for fullname length calculator

--- 4.2 Passing Data/index.js
+++ 4.2 Passing Data/index.js
@@ -4,22 +4,18 @@ import bodyParser from "body-parser";
 const app = express();
 const port = 3000;
 
-var heading = "<h1>Enter your name below</h1>";
-
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-  res.render("index.ejs", { heading });
+  res.render("index.ejs");
 });
 
 app.post("/submit", (req, res) => {
   const fNameLength = req.body.fName.length;
   const lNameLength = req.body.lName.length;
   const fullNameLength = fNameLength + lNameLength;
-  heading = `<h1>There are ${fullNameLength} letters in your name.</h1>`;
 
-  res.render("index.ejs", { heading });
-  heading = "<h1>Enter your name below</h1>";
+  res.render("index.ejs", { nameLength: fullNameLength });
 });
 
 app.listen(port, () => {
